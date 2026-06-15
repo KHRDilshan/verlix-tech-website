12a665ac5ba44f3dbcbe2f480394a1c163df4f0b
sachin20731 <munasinghe20731@gmail.com>
2026-06-15 17:08:11 +0530

    change

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -64,6 +64,7 @@ export default function ContactPage() {
               <Phone size={28} />
             </div>
             <h3 className="text-lg font-bold mb-2">Phone</h3>
+             <p className="text-foreground/70 mb-1">+94 70 233 7467</p>
             <p className="text-foreground/70 mb-1">+94 77 307 6676</p>
              
             {/* <p className="text-sm text-foreground/60">9AM-6PM EST</p> */}
